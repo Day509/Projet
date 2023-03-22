1c7b911df98af8b587029c2e8b854922b4d57610
Day509 <72015298+Day509@users.noreply.github.com>
2023-03-22 14:12:19 +0100

    Ajout du dossier modèle qui contient les classes

---
+++ Model/Chambre.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Chambre {
+
+    /**
+     * Default constructor
+     */
+    public Chambre() {
+    }
+
+    /**
+     * 
+     */
+    public int idChambre;
+
+    /**
+     * 
+     */
+    public int Etage;
+
+    /**
+     * 
+     */
+    public double Prix;
+
+    /**
+     * 
+     */
+    public int nbrLits;
+
+}
---
+++ Model/Client.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Client {
+
+    /**
+     * Default constructor
+     */
+    public Client() {
+    }
+
+    /**
+     * 
+     */
+    public String nom;
+
+    /**
+     * 
+     */
+    public String prenom;
+
+    /**
+     * 
+     */
+    public int budget;
+
+}
