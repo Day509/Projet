df21401513aa45b156eb152ed51ab950077bc90a
mehdi tazerouti <115697274+mehditazerouti@users.noreply.github.com>
2023-04-03 03:12:00 +0200

    Add files via upload

---
+++ Model/Consommation.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class Consommation {
+
+    public String produit;
+    public double prix;
+    public int quantite;
+
+
+
+    public Consommation(String produit,double prix,int quantite) {
+        this.produit=produit;
+        this.prix=prix;
+        this.quantite=quantite;
+    }
+
+    
+    
+    
+
+}
