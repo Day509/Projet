9d5bd5238f8714a57028eedac9d51c55a0c09410
mehdi tazerouti <115697274+mehditazerouti@users.noreply.github.com>
2023-04-05 01:13:26 +0200

    Update Consommation.java

--- Model/Consommation.java
+++ Model/Consommation.java
@@ -1,31 +1,44 @@
 
 import java.util.*;
 
-/**
- * 
- */
 public class Consommation {
 
-    /**
-     * Default constructor
-     */
-    public consommation(String produit,Double prix, int qte){
+    public String produit;
+    public double prix;
+    public int quantite;
 
+
+
+    public Consommation(String produit,double prix,int quantite) {
+        this.produit=produit;
+        this.prix=prix;
+        this.quantite=quantite;
     }
 
-    /**
-     * 
-     */
-    public String Produit;
+    
+    public String getProduit() {
+        return produit;
+    }
 
-    /**
-     * 
-     */
-    public double Prix;
+    public void setProduit(String produit) {
+        this.produit = produit;
+    }
 
-    /**
-     * 
-     */
-    public int Quantite;
 
-}
+    public Double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Double prix) {
+        this.prix = prix;
+    }
+
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+    
