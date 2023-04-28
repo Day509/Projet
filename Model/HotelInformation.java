aaa2c9f849ecc8cfcf2cd6ded9850c208bbdb1f2
Harish <79702468+harrypepette@users.noreply.github.com>
2023-04-28 18:41:58 +0200

    Add files via upload

---
+++ Model/HotelInformation.java
@@ -0,0 +1,57 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class HotelInformation {
+
+    private List<JTextField> list = new ArrayList<JTextField>();
+    private JPanel panel = new JPanel();
+    private JButton validateBtn = new JButton("Valider");
+    
+
+    public static void main(String args[]) {
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                HotelInformation id = new HotelInformation();
+                id.create("Informations sur l'hôtel");
+            }
+        });
+    }
+
+    private void addField(String name) {
+        JTextField jtf = new JTextField(16);
+        panel.add(new JLabel(name, JLabel.LEFT));
+        panel.add(jtf);
+        list.add(jtf);
+    }
+
+    private void create(String strTitle) {
+        panel.setLayout(new GridLayout(0, 1));
+        addField("Nom de l'hôtel:");
+        addField("Adresse de l'hôtel:");
+        addField("Nombre de chambres:");
+        addField("Nombre d'étages:");
+
+        JPanel btnPanel = new JPanel(new BorderLayout());
+        btnPanel.add(validateBtn, BorderLayout.CENTER);
+
+        JFrame frmtest = new JFrame(strTitle);
+        frmtest.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmtest.add(panel, BorderLayout.NORTH);
+        frmtest.add(btnPanel, BorderLayout.SOUTH);
+        frmtest.pack();
+        frmtest.setLocationRelativeTo(null);
+        frmtest.setVisible(true);
+        list.get(0).requestFocusInWindow();
+    }
+
+}
