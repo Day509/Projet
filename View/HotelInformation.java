90aa57c6d09ff61c2e9f0935d65de94d70027140
Harish <79702468+harrypepette@users.noreply.github.com>
2023-05-06 11:17:03 +0200

    Add files via upload

--- View/HotelInformation.java
+++ View/HotelInformation.java
@@ -1,53 +1,79 @@
-package View;
-
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 
 public class HotelInformation {
 
     private List<JTextField> list = new ArrayList<JTextField>();
-    private JPanel panel = new JPanel();
-    private JButton validateBtn = new JButton("Valider");
-    
+    private JPanel panel = new JPanel(new GridBagLayout());
 
     public static void main(String args[]) {
-        EventQueue.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                HotelInformation id = new HotelInformation();
-                id.create("Informations sur l'hôtel");
-            }
+        EventQueue.invokeLater(() -> {
+            HotelInformation id = new HotelInformation();
+            id.create("Informations sur l'hôtel");
         });
     }
-
-    private void addField(String name) {
+    
+    private void addField(String name, int gridx, int gridy) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.LINE_START;
+        c.gridx = gridx;
+        c.gridy = gridy;
+        c.insets.top = 10;
+        panel.add(new JLabel(name), c);
         JTextField jtf = new JTextField(16);
-        panel.add(new JLabel(name, JLabel.LEFT));
-        panel.add(jtf);
+        c.gridx = gridx + 1;
+        c.insets.left = 10;
+        panel.add(jtf, c);
         list.add(jtf);
     }
+    
+    private void create(String strTitle) {
 
-    void create(String strTitle) {
-        panel.setLayout(new GridLayout(0, 1));
-        addField("Nom de l'hôtel:");
-        addField("Adresse de l'hôtel:");
-        addField("Nombre de chambres:");
-        addField("Nombre d'étages:");
-
-        JPanel btnPanel = new JPanel(new BorderLayout());
-        btnPanel.add(validateBtn, BorderLayout.CENTER);
-
-        JFrame frmtest = new JFrame(strTitle);
-        frmtest.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frmtest.add(panel, BorderLayout.NORTH);
-        frmtest.add(btnPanel, BorderLayout.SOUTH);
-        frmtest.pack();
-        frmtest.setLocationRelativeTo(null);
-        frmtest.setVisible(true);
-        list.get(0).requestFocusInWindow();
+        JLabel label = new JLabel("Création d'hotel");
+        label.setFont(new Font("Arial", Font.BOLD, 16));
+        GridBagConstraints cLabel = new GridBagConstraints();
+        cLabel.gridx = 0;
+        cLabel.gridy = 0;
+        cLabel.gridwidth = 2;
+        cLabel.insets.bottom = 50;
+        cLabel.anchor = GridBagConstraints.CENTER;
+        panel.add(label, cLabel);
+      
+        // Ajouter les champs texte pour les informations de l'hôtel
+        addField("Nom de l'hôtel:", 0, 1);
+        addField("Adresse de l'hôtel:", 0, 2);
+        addField("Nombre de chambres:", 0, 3);
+        addField("Nombre d'étages:", 0, 4);
+    
+        // Ajouter le bouton Valider
+        JButton validerBtn = new JButton("Valider");
+        GridBagConstraints cButton = new GridBagConstraints();
+        cButton.gridx = 0;
+        cButton.gridy = 5;
+        cButton.gridwidth = 2;
+        cButton.insets.top = 20;
+        cButton.anchor = GridBagConstraints.CENTER;
+        panel.add(validerBtn, cButton);
+    
+        // Créer la fenêtre et l'afficher
+        JFrame frame = new JFrame(strTitle);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(500, 300));
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
-
 }
