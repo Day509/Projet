f24189fb0d9082847566d831e6e605ffe1a1db4c
mehdi tazerouti <115697274+mehditazerouti@users.noreply.github.com>
2023-05-10 19:08:12 +0200

    Add files via upload

--- Controller/SideBarControl.java
+++ Controller/SideBarControl.java
@@ -1,45 +1,72 @@
 package Controller;
-
+    
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-import View.ReservPage;
-import View.RoomPage;
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
-public class SideBarControl implements ActionListener {
+import View.*;
 
-    private JPanel mainContent;
-    private JPanel reservPanel = new ReservPage();
-    private JPanel rooms = new RoomPage();
+public class SideBarControl implements ActionListener{
 
-    public SideBarControl(JPanel mainContent) {
-        this.mainContent = mainContent;
-    }
+    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();//recuperer la dimension de l'ecrant
+    //TableauDeBord TableauDeBord=new TableauDeBord(dim.height, dim.width);
+    //RoomPage RoomPage =new RoomPage();
+    //ReservPage ReservPage =new ReservPage();
+    JButton dashButton;
+    JButton roomButton;
+    JButton reservButton;
+    JButton youHotelButton;
+    Dashboard Dashboard;
+    JPanel section1;
+    JPanel section2;
+    JPanel section3;
+    //JPanel mainContent;
+
+    public SideBarControl(JButton dashButton,JButton roomButton,JButton reservButton,JButton youHotelButton
+    ,JPanel section1,JPanel section2,JPanel section3) {
+        this.dashButton = dashButton;
+        this.roomButton = roomButton;
+        this.reservButton = reservButton;
+        this.youHotelButton = youHotelButton;
+        this.section1 = section1;
+        this.section2 = section2;
+        this.section3 = section3;
+     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton) e.getSource();
-        switch (button.getText()) {
-            case "Tableau de bord":
-                reservPanel.setVisible(false);
-                rooms.setVisible(false);
-                mainContent.setVisible(true);
-                break;
-            case "Chambres":
-                // Code pour changer le mainContent pour les chambres
-                break;
-            case "Réservations":
-                // Code pour changer le mainContent pour les réservations
-                break;
-            case "Votre hôtel":
-                // Code pour changer le mainContent pour votre hôtel
-                break;
-            default:
-                break;
+        if (button == dashButton) {
+            // Faire quelque chose lorsque le bouton 1 est cliqué
+            section2.setVisible(false);
+            section3.setVisible(false);
+            section1.setVisible(true);
+            //mainContent=section1;
+            
+            //section4.hide();
+        } else if (button == roomButton) {
+            // Faire quelque chose lorsque le bouton 2 est cliqué
+            section1.setVisible(false);
+            section3.setVisible(false);
+            section2.setVisible(true);
+            
+            //section3.setVisible(false);
+            //section2.setVisible(true);
+           
+        } else if (button == reservButton) {
+            // Faire quelque chose lorsque le bouton 3 est cliqué
+            section1.setVisible(false);
+            section2.setVisible(false);
+            section3.setVisible(true);
+           
+        } else if (button == youHotelButton) {
+            // Faire quelque chose lorsque le bouton 4 est cliqué
+           
         }
     }
-
 }
