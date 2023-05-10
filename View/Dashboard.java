032c51b5e1c3a071de4968ffc7b5b24315b23e28
mehdi tazerouti <115697274+mehditazerouti@users.noreply.github.com>
2023-05-10 19:10:34 +0200

    Add files via upload

--- View/Dashboard.java
+++ View/Dashboard.java
@@ -1,20 +1,45 @@
 package View;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-
+import Controller.*;
 public class Dashboard extends JPanel {
 
     int height;
     int width;
-    JPanel mainContent;
-    
+    //JPanel panelContainer;
 
+    // Initialisation des sections
+    JPanel section1 = new TableauDeBord( height, width);
+    JPanel section2 = new RoomPage();
+    JPanel section3 = new ReservPage();
+    //section4 = new Section4();
+
+    JPanel panelContainer;
+
+    
+    JButton dashButton;
+    JButton roomButton;
+    JButton reservButton;
+    JButton youHotelButton;
+
+        
 
     public Dashboard(int h, int w) {
         height = h;
         width = w;
-        mainContent = new RoomPage();
+        // Affichage de la section 1 par défaut
+        //mainContent=section1;
+        panelContainer = new JPanel();
+        panelContainer.setLayout(new CardLayout());
+        panelContainer.add(section1);
+        panelContainer.add(section2);
+        panelContainer.add(section3);
+    //j'affiche le tableau de brod par defaut
+        section1.setVisible(true);
+
+        
 
         JPanel panel = showDashSection();
         int w_dash = w / 2 + 400;
@@ -24,12 +49,20 @@ public class Dashboard extends JPanel {
         this.add(panel);
     }
 
+
     private JPanel showDashSection() {
         JPanel panel = new JPanel(new GridBagLayout());
         panel.setBackground(new Color(255, 255, 255, 215));
 
         JPanel sideBar = sidebar();
+         //gestion des boutttons de la sidebare
+        SideBarControl SideBarControl=new SideBarControl(dashButton,roomButton,reservButton,youHotelButton,section1,section2,section3);
+        dashButton.addActionListener(SideBarControl);
+        roomButton.addActionListener(SideBarControl);
+        reservButton.addActionListener(SideBarControl);
+        youHotelButton.addActionListener(SideBarControl);
 
+    
         JPanel wPanel = new WeekPanel();
 
         GridBagConstraints gbcSideBar = new GridBagConstraints();
@@ -54,9 +87,14 @@ public class Dashboard extends JPanel {
         gbcmainContent.fill = GridBagConstraints.BOTH; // remplissage
 
         gbcmainContent.weightx = 1.0; // poid en x
-        panel.add(mainContent, gbcmainContent); // ajout du panel au panel principal
+        panel.add(panelContainer, gbcmainContent); // ajout du panel au panel principal
 
+        //panel.add(section2, gbcmainContent);
+        //panel.add(section3, gbcmainContent);
         
+
+
+       
 
         return panel;
 
@@ -68,10 +106,14 @@ public class Dashboard extends JPanel {
         panel.setBackground(new Color(51, 70, 88, 127));
         panel.setPreferredSize(new Dimension(200, 400));
 
-        JButton dashButton = new JButton("Tableau de bord");
-        JButton roomButton = new JButton("Chambres");
-        JButton reservButton = new JButton("Réservations");
-        JButton youHotelButton = new JButton("Votre hôtel");
+         dashButton = new JButton("Tableau de bord");
+        //this.dashButton=dashButton;
+         roomButton = new JButton("Chambres");
+        //this.roomButton=roomButton;
+         reservButton = new JButton("Réservations");
+        //this.reservButton=reservButton;
+         youHotelButton = new JButton("Votre hôtel");
+        //this.youHotelButton=youHotelButton;
 
         // Ajout des boutons au sidebar
         panel.add(dashButton);
@@ -83,8 +125,7 @@ public class Dashboard extends JPanel {
         return panel;
     }
 
-    public JPanel getRoomPage() {
-        return mainContent;
-    }
+
+
 
 }
