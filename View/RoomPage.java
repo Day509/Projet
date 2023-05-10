3c25dadfd64b8eab88f1cc854b4729737207263d
Davidson Charlot <72015298+Day509@users.noreply.github.com>
2023-05-10 21:14:20 +0200

    Add files via upload

--- View/RoomPage.java
+++ View/RoomPage.java
@@ -9,54 +9,59 @@ import java.time.LocalDate;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 public class RoomPage extends JPanel {
 
     private final int NB_CHAMBRES = 10; // nombre de chambres
-    private final int NB_JOURS = 7; // nombre de jours à afficher
-    private final String[] JOURS_SEMAINE = {"Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"}; // jours de la semaine
+    private final int NB_JOURS = 8; // nombre de jours à afficher
+    private final String[] JOURS_SEMAINE = {"", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim" }; // jours de la semaine
     private final int TAILLE_CASE = 40; // taille des cases du tableau
-    
+    LocalDate date;
+    JTable table;
+
     public RoomPage() {
         setLayout(new BorderLayout());
-        
-        // création du panneau pour les numéros de chambre
-        JPanel chambrePanel = new JPanel(new GridLayout(NB_CHAMBRES, 1));
-        chambrePanel.setPreferredSize(new Dimension(50, NB_CHAMBRES * TAILLE_CASE));
-        chambrePanel.setBackground(Color.WHITE);
-        for (int i = 1; i <= NB_CHAMBRES; i++) {
-            JLabel label = new JLabel("Chambre " + i, JLabel.CENTER);
-            label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            chambrePanel.add(label);
-        }
-        
-        // création du panneau pour le tableau
-        JPanel tableauPanel = new JPanel(new GridLayout(NB_CHAMBRES + 1, NB_JOURS));
-        tableauPanel.setBackground(Color.WHITE);
+        // création du panneau pour le taableau
         LocalDate date = LocalDate.now();
-        
+        DefaultTableModel model = new DefaultTableModel(NB_CHAMBRES, NB_JOURS);
+        model.setColumnIdentifiers(JOURS_SEMAINE);
         // ajout des jours de la semaine dans la première ligne
-        for (int i = 0; i < NB_JOURS; i++) {
-            JLabel label = new JLabel(JOURS_SEMAINE[(date.getDayOfWeek().getValue() - 1 + i) % 7] + " " + date.plusDays(i).getDayOfMonth() + "/" + date.plusDays(i).getMonthValue(), JLabel.CENTER);
-            label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            label.setPreferredSize(new Dimension(TAILLE_CASE, TAILLE_CASE));
-            label.setFont(new Font("SansSerif", Font.PLAIN, 12));
-            tableauPanel.add(label);
-        }
         
+
         // ajout des cellules pour chaque chambre
-        for (int i = 1; i <= NB_CHAMBRES; i++) {
-            for (int j = 0; j < NB_JOURS; j++) {
-                JLabel label = new JLabel();
-                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                label.setPreferredSize(new Dimension(TAILLE_CASE, TAILLE_CASE));
-                label.setOpaque(true);
-                label.setBackground(Color.PINK);
-                tableauPanel.add(label);
+        for (int i = 0; i < NB_CHAMBRES; i++) {
+            model.setValueAt("Chambre " + (i+1), i, 0);
+            for (int j = 1; j < NB_JOURS; j++) {
+                model.setValueAt("", i, j);
             }
         }
         
-        add(chambrePanel, BorderLayout.WEST);
-        add(tableauPanel, BorderLayout.CENTER);
+        
+
+        // création du tableau avec le modèle
+        table = new JTable(model);
+        table.setRowHeight(TAILLE_CASE);
+        table.getTableHeader().setPreferredSize(new Dimension(0, TAILLE_CASE));
+        table.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            public void setValue(Object value) {
+                setText((value == null) ? "" : value.toString());
+            }
+        });
+
+        // ajout du tableau dans un JScrollPane
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension((NB_JOURS + 1) * TAILLE_CASE, NB_CHAMBRES * TAILLE_CASE));
+
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    LocalDate setDate(LocalDate date) {
+        this.date = date;
+        return date;
     }
 }
